8df0107ae32284586b70596de45641b75976c454
Kira Nim <kiras.nim@gmail.com>
2021-11-20 22:41:01 +0100

    WIP

--- app/src/main/java/com/example/android_mandatory/ui/EditSymptomActivity.java
+++ app/src/main/java/com/example/android_mandatory/ui/EditSymptomActivity.java
@@ -1,6 +1,14 @@
 package com.example.android_mandatory.ui;
 
-public class EditSymptomActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.android_mandatory.R; // Hvis R driller så husk lige denne her :D
+import android.os.Bundle;
 
+public class EditSymptomActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_symptom);
+    }
 }
--- app/src/main/java/com/example/android_mandatory/ui/SymptomsFragment.java
+++ app/src/main/java/com/example/android_mandatory/ui/SymptomsFragment.java
@@ -1,13 +1,10 @@
 package com.example.android_mandatory.ui;
 
-import android.content.Intent;
 import android.graphics.Canvas;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
@@ -23,7 +20,6 @@ import com.example.android_mandatory.R;
 import com.example.android_mandatory.databinding.FragmentSymptomsBinding;
 import java.util.ArrayList;
 import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
-
 
 
 public class SymptomsFragment extends Fragment{
